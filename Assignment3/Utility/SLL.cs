1e4129d5b223dea0a6bb4227ab4c7cd4beb36e72
Chris Williams <chris_williams@domain.ext>
2024-04-06 15:51:26 -0600

    Add project files.

---
+++ Assignment3/Utility/SLL.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    public class SLL : ILinkedListADT
+    {
+        public Node head;
+        public int size;
+
+        public SLL()
+        {
+            head = null; size = 0;
+        }
+        public bool IsEmpty()
+        {
+            return size == 0;
+        }
+        public void Clear()
+        {
+            head = null;
+            size = 0;
+        }
+        public void AddLast(User value)
+        {
+            if (IsEmpty())
+            {
+                head = new Node(value);
+            }
+            else
+            {
+                Node current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = new Node(value);
+            }
+            size++;
+        }
+        public void AddFirst(User value)
+        {
+            Node newNode = new Node(value);
+            newNode.Next = head;
+            head = newNode;
+            size++;
+        }
+        public void Add(User value, int index)
+        {
+            if (index < 0 || index > size)
+            {
+                throw new IndexOutOfRangeException("Index is out of range.");
+            }
+
+            if (index == 0)
+            {
+                AddFirst(value);
+            }
+            else if (index == size)
+            {
+                AddLast(value);
+            }
+            else
+            {
+                Node current = head;
+                for (int i = 0; i < index - 1; i++)
+                {
+                    current = current.Next;
+                }
+                Node newNode = new Node(value);
+                newNode.Next = current.Next;
+                current.Next = newNode;
+                size++;
+            }
+        }
+        public void Replace(User value, int index)
+        {
+            if (index < 0 || index >= size)
+            {
+                throw new IndexOutOfRangeException("Index is out of range.");
+            }
+
+            Node current = head;
+            for (int i = 0; i < index; i++)
+            {
+                current = current.Next;
+            }
+            current.Value = value;
+        }
+        public int Count()
+        {
+            return size;
+        }
+
+        public void RemoveFirst()
+        {
+            if (IsEmpty())
+            {
+                throw new CannotRemoveException("List is empty.");
+            }
+
+            head = head.Next;
+            size--;
+        }
+        public void RemoveLast()
+        {
+            if (IsEmpty())
+            {
+                throw new CannotRemoveException("List is empty.");
+            }
+
+            if (size == 1)
+            {
+                head = null;
+            }
+            else
+            {
+                Node current = head;
+                while (current.Next.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = null;
+            }
+            size--;
+        }
+        public void Remove(int index)
+        {
+            if (index < 0 || index >= size)
+            {
+                throw new IndexOutOfRangeException("Index is out of range.");
+            }
+
+            if (index == 0)
+            {
+                RemoveFirst();
+            }
+            else if (index == size - 1)
+            {
+                RemoveLast();
+            }
+            else
+            {
+                Node current = head;
+                for (int i = 0; i < index - 1; i++)
+                {
+                    current = current.Next;
+                }
+                current.Next = current.Next.Next;
+                size--;
+            }
+
+        }
+        public User GetValue(int index)
+        {
+            if (index < 0 || index >= size)
+            {
+                throw new IndexOutOfRangeException("Index is out of range.");
+            }
+
+            Node current = head;
+            for (int i = 0; i < index; i++)
+            {
+                current = current.Next;
+            }
+            return current.Value;
+        }
+        public int IndexOf(User value)
+        {
+            Node current = head;
+            int index = 0;
+            while (current != null)
+            {
+                if (current.Value.Equals(value))
+                {
+                    return index;
+                }
+                current = current.Next;
+                index++;
+            }
+            return -1;
+        }
+        public bool Contains(User value)
+        {
+            return IndexOf(value) != -1;
+        }
+        public void SortByName()
+        {
+            List<User> userList = new List<User>();
+            Node current = head;
+            while (current != null)
+            {
+                userList.Add(current.Value);
+                current = current.Next;
+            }
+
+            userList = userList.OrderBy(u => u.Name).ToList();
+
+            Clear();
+            foreach (User user in userList)
+            {
+                AddLast(user);
+            }
+        }
+
+    }
+}
---
+++ Assignment3Tests/Utility/SLLTests.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.Tests
+{
+    [TestClass()]
+    public class SLLTests
+    {
+        [TestMethod()]
+        public void IsEmptyTest()
+        {
+            var empty = new SLL();
+            bool result = empty.IsEmpty();
+            Assert.IsTrue(result);
+        }
+        [TestMethod()]
+        public void IsClear()
+        {
+            var empty = new SLL();
+            empty.Clear();
+            bool result = empty.IsEmpty();
+            Assert.IsTrue(result);
+        }
+        [TestMethod()]
+        public void AddFirstTest()
+        {
+            var list = new SLL();
+            User user = new User(1, "Bob", "bob@example.com", "password456");
+            list.AddFirst(user);
+
+            Assert.AreEqual("Bob", list.head.Value.Name);
+            Assert.AreEqual(1, list.Count());
+        }
+        [TestMethod()]
+        public void AddLastTest()
+        {
+            var list = new SLL();
+            User user1 = new User(1, "Bob", "bob22@gmail.com", "password22");
+            User user2 = new User(2,"Alice", "alice@example.com", "password123");
+
+            list.AddLast(user1);
+            list.AddLast(user2);
+
+            Assert.AreEqual("Alice", list.head.Next.Value.Name);
+            Assert.AreEqual(2, list.Count());
+        }
+        [TestMethod()]
+        public void AddAtIndexTest()
+        {
+            var list = new SLL();
+            User user1 = new User(1, "Alice", "alice@example.com", "password123");
+            User user2 = new User(2, "Bob", "bob@example.com", "password456");
+            User user3 = new User(3, "Charlie", "charlie@example.com", "password789");
+
+            list.AddLast(user1);
+            list.AddLast(user3);
+            list.Add(user2, 1);
+
+            Assert.AreEqual("Alice", list.GetValue(0).Name);
+            Assert.AreEqual("Bob", list.GetValue(1).Name);
+            Assert.AreEqual("Charlie", list.GetValue(2).Name);
+            Assert.AreEqual(3, list.Count());
+
+        }
+        [TestMethod()]
+        public void ReplaceTest()
+        {
+            var list = new SLL();
+            User user = new User(1, "Alice", "alice@example.com", "password123");
+            list.AddLast(user);
+
+            User newUser = new User(1, "Bob", "bob@example.com", "password456");
+            list.Replace(newUser, 0);
+
+            Assert.AreEqual("Bob", list.GetValue(0).Name);
+        }
+        [TestMethod()]
+        public void RemoveFirstTest()
+        {
+            var list = new SLL();
+            User user1 = new User(1, "Alice", "alice@example.com", "password123");
+            User user2 = new User(2, "Bob", "bob@example.com", "password456");
+
+            list.AddLast(user1);
+            list.AddLast(user2);
+            list.RemoveFirst();
+
+            Assert.AreEqual("Bob", list.GetValue(0).Name);
+            Assert.AreEqual(1, list.Count());
+        }
+        [TestMethod()]
+        public void RemoveLastTest()
+        {
+            var list = new SLL();
+            User user1 = new User(1, "Alice", "alice@example.com", "password123");
+            User user2 = new User(2, "Bob", "bob@example.com", "password456");
+
+            list.AddLast(user1);
+            list.AddLast(user2);
+            list.RemoveLast();
+
+            Assert.AreEqual("Alice", list.GetValue(0).Name);
+            Assert.AreEqual(1, list.Count());
+        }
+        [TestMethod()]
+        public void RemoveAtIndexTest()
+        {
+            var list = new SLL();
+            User user1 = new User(1, "Alice", "alice@example.com", "password123");
+            User user2 = new User(2, "Bob", "bob@example.com", "password456");
+            User user3 = new User(3, "Charlie", "charlie@example.com", "password789");
+
+            list.AddLast(user1);
+            list.AddLast(user2);
+            list.AddLast(user3);
+            list.Remove(1); 
+
+            Assert.AreEqual("Alice", list.GetValue(0).Name);
+            Assert.AreEqual("Charlie", list.GetValue(1).Name);
+            Assert.AreEqual(2, list.Count());
+        }
+        [TestMethod()]
+        public void GetValueTest()
+        {
+            var list = new SLL();
+            User user1 = new User(1, "Alice", "alice@example.com", "password123");
+            User user2 = new User(2, "Bob", "bob@example.com", "password456");
+
+            list.AddLast(user1);
+            list.AddLast(user2);
+            User user = list.GetValue(1);
+
+            Assert.AreEqual("Bob", user.Name);
+        }
+        [TestMethod()]
+        public void SortByNameTest()
+        {
+            var list = new SLL();
+            User user1 = new User(1, "Bob", "bob@example.com", "password456");
+            User user2 = new User(2, "Alice", "alice@example.com", "password123");
+            User user3 = new User(3, "Charlie", "charlie@example.com", "password789");
+
+            list.AddLast(user1);
+            list.AddLast(user2);
+            list.AddLast(user3);
+            list.SortByName();
+
+            Assert.AreEqual("Alice", list.GetValue(0).Name);
+            Assert.AreEqual("Bob", list.GetValue(1).Name);
+            Assert.AreEqual("Charlie", list.GetValue(2).Name);
+        }
+    }
+}
